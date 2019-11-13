d74ac8accaf4bbe2fb66b6e0c51805883097ff55
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-11-13 19:52:04 +0000

    add bike and bike_spec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,21 @@
 class DockingStation
-  def release_bike
+
+  def initialize
+
   end
+
+  def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
+    bike
+  end
+
+  def bike
+    
+  end
+
 end
+
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,15 @@
 require "docking_station"
 
-
 describe DockingStation do
-  it "responds to the method release_bike" do
-    expect(subject).to respond_to(:release_bike)
-  end  
+
+  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
 end
 
