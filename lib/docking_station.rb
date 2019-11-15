29ad8eb2f9d2270b51bb53661c5fbcda33347c52
Ellie <elliemmanifold@gmail.com>
2019-11-15 15:12:42 +0000

    Change docking station to an array of bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,21 @@
+
 class DockingStation
 
-  attr_reader :bike
+  attr_accessor :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    @bike ? @bike : (fail "There are no bikes")
+    fail "There are no bikes" if @bikes.empty?
+    return @bikes[0]
+    @bikes.pop
   end
 
   def dock(bike)
-    raise "docking station full"
-    @bike = bike
+    fail "docking station full" if @bikes.length >= 19
+    @bikes << bike
   end
 
 end
